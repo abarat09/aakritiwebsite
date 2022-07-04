0fe1c94c4c7b0f763c65b620197120e6fafeafcc
Derek Foley <foley.derekj@gmail.com>
2022-07-04 17:02:47 -0400

    aakriti website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import HomePage from "./components/Homepage";
+import HomePage from "./components/Homepage";
 
 function App() {
   return (
     <div style={{ height: '100%', width: '100%', margin: 0, padding: 0,backgroundColor: '#DCD7C9',
   }} className="App">
-      {/* <HomePage /> */}
-      <p>Hello</p>
+      <HomePage />
     </div>
   );
 }

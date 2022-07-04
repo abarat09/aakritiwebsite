331c7c8d86043351e14abf3cb38a10d6e2c1f198
Derek Foley <foley.derekj@gmail.com>
2022-07-04 16:37:43 -0400

    aakriti website

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root');
-render(
+ReactDOM.render(
     <App style={{padding: 0}}/ >, root
 );
 
